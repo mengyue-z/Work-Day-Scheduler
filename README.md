adebc4d773312b16ab6931e92c5de58a1c64143b
Mengyue Zhang <mengyue.z@outlook.com>
2020-12-15 00:02:49 -0500

    updated read me file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Work-Day-Scheduler
+# Work Day Scheduler
+
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+# Description
+
+This is a simple calendar application that allows a user to save events for each working hour of the day.
+
+User can type in the tasks for each hour and then click the save button on the right so the tasks will be saved and are accessbile anytime.
+
+
+
+# Screenshots
+
+The following image shows this calendar application's appearance and functionality:
+
+![password generator homework demo](./screenshots/image1.jpg)
+![password generator homework demo](./screenshots/image2.jpg)
+
+# Links
+
+* The URLs of the deployed website: https://mengyue-z.github.io/Work-Day-Scheduler/
+
+* The URLs of the GitHub repositories: https://github.com/mengyue-z/Work-Day-Scheduler
+
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // current Day
 $("#currentDay").text(dayjs().format('dddd, MMMM D'));
-var d = new Date();
-var currentHour = d.getHours();
+
+    var d = new Date();
+    var currentHour = d.getHours();
+
+// generate timeblocks
 for (i=0; i<9;i++ ) {
     var blockDiv = $("<div>").addClass("row")
     var taskInputBlock = $("<input>").addClass("description")
@@ -16,12 +19,16 @@ for (i=0; i<9;i++ ) {
     }
 
     if((9+i)<12){
-        hourP.text((9+i)+"AM")
+        hourP.text((9+i)+"AM");
+        hourP.addClass(""+(9+i));
     } else if((9+i===12)){
-        hourP.text("12PM")
+        hourP.text("12PM");
+        hourP.addClass(""+(9+i));
     } else {
-        hourP.text((9+i-12) +"PM")
+        hourP.text((9+i-12) +"PM");
+        hourP.addClass(""+(9+i));
     }
+    
     var saveButton = $("<button>").addClass("saveBtn")
     $(".container").append(blockDiv);
     blockDiv.append(hourP);
@@ -34,13 +41,41 @@ for (i=0; i<9;i++ ) {
 }
 
 
+// click save button to save task inputs into local storage
+$(document).on("click", ".saveBtn", function () {
+var taskInputs = $(this).siblings(".description").val();
+var time = $(this).siblings(".hour").text();
+localStorage.setItem(time,taskInputs);
+});
 
-// var taskList = [];
+// append task inputs to the time blocks so that when refresh, it will still show up on the page
+function appendTasks() {
+    var nine = localStorage.getItem("9AM");
+    $(".9").siblings(".description").val(nine);
 
+    var ten = localStorage.getItem("10AM");
+    $(".10").siblings(".description").val(ten);
 
-// $(".saveBtn").on("click", function () {
-//     var taskInputs = $("#taskInput").val();
-//     taskList.push(taskInputs);
-//     console.log(taskList);
-// });
+    var eleven = localStorage.getItem("11AM");
+    $(".11").siblings(".description").val(eleven);
 
+    var twelve = localStorage.getItem("12PM");
+    $(".12").siblings(".description").val(twelve);
+
+    var one = localStorage.getItem("1PM");
+    $(".13").siblings(".description").val(one);
+
+    var two = localStorage.getItem("2PM");
+    $(".14").siblings(".description").val(two);
+
+    var three = localStorage.getItem("3PM");
+    $(".15").siblings(".description").val(three);
+
+    var four = localStorage.getItem("4PM");
+    $(".16").siblings(".description").val(four);
+
+    var five = localStorage.getItem("5PM");
+    $(".17").siblings(".description").val(five);
+}
+
+appendTasks();
